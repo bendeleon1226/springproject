d43aa31fe4197a1ec5f5c4bef332057b330dcb17
bendeleon1226 <benedicto.gualda.deleon@gmail.com>
2016-05-24 15:43:45 +0800

    Spring exampls of prototype scope

---
+++ SpringExamplePrototype/src/com/prototype/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.prototype;
+
+public class HelloWorld {
+	private String message;
+
+	public String getMessage() {
+		System.out.println("Ben Message: " + message);
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
