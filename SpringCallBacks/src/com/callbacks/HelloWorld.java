e530a6e6b335d552282a2ed986dfbfddcd873f38
bendeleon1226 <benedicto.gualda.deleon@gmail.com>
2016-05-24 18:08:10 +0800

    Example of Spring Callback

---
+++ SpringCallBacks/src/com/callbacks/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.callbacks;
+
+public class HelloWorld {
+	private String message;
+
+	public String getMessage() {
+		System.out.println("Ben Message: " + message);
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public void benInit(){
+		System.out.println("Bean is going through initialization!");
+	}
+	
+	public void destroy(){
+		System.out.println("Bean will destroy now!");
+	}
+
+}
